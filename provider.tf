65ee76f29d15832300675d8bd0390a5387d488b2
Leandro <118134461+leandroc97@users.noreply.github.com>
2023-10-13 22:13:34 +0100

    Added 3 files
    New small project that creates subnets per AZ

---
+++ provider.tf
@@ -0,0 +1,13 @@
+# Configuration of the AWS provider with an input variable
+provider "aws" {
+  region = var.region
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
