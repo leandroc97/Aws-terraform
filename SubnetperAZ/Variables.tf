617cfb1c4fc5d0669366e919f4f025cda557b044
Leandro <118134461+leandroc97@users.noreply.github.com>
2023-10-15 13:54:44 +0100

    Added 3 files for the SubnetPerAZ project

---
+++ SubnetperAZ/Variables.tf
@@ -0,0 +1,8 @@
+/*
+Variables file - Only one input variable created. This is region variable to be defined by the user
+*/
+
+variable "region" {
+  description = "Please enter the AWS region to provision the infrastructure"
+  type        = string
+}
