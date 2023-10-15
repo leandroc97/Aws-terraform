77ddfb3cf275d139c911e568bb2dc8138678fb7c
Leandro <118134461+leandroc97@users.noreply.github.com>
2023-10-15 13:56:28 +0100

    Delete Variables.tf

--- Variables.tf
+++
@@ -1,8 +0,0 @@
-/*
-Variables file - Only one input variable created. This is region variable to be defined by the user
-*/
-
-variable "region" {
-  description = "Please enter the AWS region to provision the infrastructure"
-  type        = string
-}
